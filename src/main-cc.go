478d70f9d77530709de59bb8102326f2cbd6dbd0
Henri <104494267+H1nr1@users.noreply.github.com>
2023-08-14 16:44:28 -0400

    Update main-cc.go

--- src/main-cc.go
+++ src/main-cc.go
@@ -8,7 +8,9 @@
 
 {{/* Configurable Values */}}
 {{ $CountTwice := false }} {{/* Allow users to count multiple times in a row; true/false */}}
-{{ $RoleID := false }} {{/* Counting role ID; set to false to disable */}}
+{{ $CorrectRID := false }} {{/* Correct Counting role ID; set to false to disable */}}
+{{ $IncorrectRID := flase }} {{/* Incorrect Counting role ID; set to false to disable */}}
+{{ $ErrorCID := 123 }} {{/* Channel ID to send errors to */}}
 {{ $SecondChance := true }} {{/* Second chance if wrong; true/false */}}
 {{ $StatsCC := true }} {{/* If you added the Stats CC; true/false */}}
 {{ $Reactions := true }} {{/* Allow confirmative reactions on message; true false */}}
@@ -19,8 +21,7 @@
 {{/* End of configurable values */}}
 
 {{/* No Touchy */}}
-{{ $db := or (dbGet 0 "Counting").Value (sdict "Last" (sdict "User" 0 "Msg" 0) "Next" 1 "HighScore" (sdict "User" 0 "Num" 0 "Time" currentTime) "SecondChance" 1) }}
-{{ $UStats := or (dbGet .User.ID "Counting").Value (sdict "Correct" 0 "Total" 0) }}
+{{ $db := or (dbGet 0 "Counting").Value (sdict "Last" (sdict "User" 204255221017214977 "Msg" 0) "Next" 1 "HighScore" (sdict "User" 204255221017214977 "Num" 1 "Time" currentTime) "SecondChance" 2) }}
 
 {{ with .ExecData }}
 	{{ if not (getMessage nil .) }} {{/* Check if number was deleted */}}
@@ -33,34 +34,47 @@
 {{ if or (ne $db.Last.User .User.ID) $CountTwice }} {{/* Checks user */}}
 	{{ if eq $db.Next $Number }} {{/* Checks if correct number */}}
 		{{ $db.Set "Next" (add $db.Next 1) }}
-		{{ if $Reactions }}
-			{{ addReactions $CorrectEmoji }}
-			{{ if and $ReactionDelete (ne $db.Last.Msg 0) }}
-				{{ deleteMessageReaction nil $db.Last.Msg 204255221017214977 $CorrectEmoji }}
+		{{ try }}
+			{{ if $Reactions }}
+				{{ addReactions $CorrectEmoji }}
+				{{ if and $ReactionDelete (ne $db.Last.Msg 0) }}
+					{{ deleteMessageReaction nil $db.Last.Msg 204255221017214977 $CorrectEmoji }}
+				{{ end }}
+				{{ if not (mod $Number 100) }}
+					{{ addReactions "💯" }}
+				{{ end }}
 			{{ end }}
+		{{ catch }}
+			{{ sendMessage $ErrorCID (print "Counting: `" .Error "`") }}
+		{{ end }}
+		{{ with $CorrectRID }} {{/* Correct Counting role */}}
+			{{ takeRoleID $db.Last.User . }}{{ giveRoleID $.User.ID . }}
 		{{ end }}
 		{{ $db.Set "Last" (sdict "User" .User.ID "Msg" .Message.ID) }}
-		{{ if $RoleID }} {{/* Counting role */}}
-			{{ takeRoleID $db.Last.User $RoleID }}{{ giveRoleID .User.ID $RoleID }}
-		{{ end }}
 		{{ if $StatsCC }} {{/* Update leaderboard values */}}
-			{{ $UStats.Set "Correct" (add $UStats.Correct 1) }}{{ $UStats.Set "Total" (add $UStats.Total 1) }}{{ dbSet .User.ID "Counting" $UStats }}
-			{{ if gt $Number $db.HighScore.Num }}{{ $db.Set "HighScore" (sdict "User" .User.ID "Num" $Number) }}{{ end }}
+			{{ $Silence := dbIncr .User.ID "CCorrect" 1 }}{{ $Silence = dbIncr .User.ID "CCount" 1 }}
+			{{ if gt $Number $db.HighScore.Num }}{{ $db.Set "HighScore" (sdict "User" .User.ID "Num" $Number "Time" currentTime) }}{{ end }}
 		{{ end }}
 		{{ dbSet 0 "Counting" $db }}
 		{{ execCC .CCID nil 10 .Message.ID }} {{/* Call to check if message was deleted */}}
 		
 	{{ else }} {{/* Wrong number */}}
 		{{ $db.Set "SecondChance" (sub $db.SecondChance 1) }}
-		{{ if and $SecondChance (eq $db.SecondChance 0) }} {{/* Second Chance */}}
-			{{ if $Reactions }}{{ addReactions $WarningEmoji }}{{ end }}
+		{{ with $CorrectRID }}{{ takeRoleID $db.Last.User . }}{{ end }}
+		{{ with $IncorrectRID }}{{ addRoleID . }}{{ removeRoleID . 259200 }}{{ end }}
+		{{ if and $SecondChance (gt $db.SecondChance 0) }} {{/* Second Chance */}}
+			{{ try }}{{ if $Reactions }}{{ addReactions $WarningEmoji }}{{ end }}
+			{{ catch }}{{ sendMessage $ErrorCID (print "Counting: `" .Error "`") }}{{ end }}
 			{{ $db.Set "Last" (sdict "User" .User.ID "Msg" .Message.ID) }}{{ dbSet 0 "Counting" $db }}
-			{{ sendMessage nil (cembed "description" (print .User.Mention " sent an incorrect number of " $Number "\n**But**, second chance saved the count!\nThe next number is " $db.Next) "color" 16744192) }}
+			{{ sendMessage nil (cembed "description" (print .User.Username " sent an incorrect number of " $Number "\n**But**, second chance saved the count!\nThe next number is " $db.Next) "color" 16744192) }}
 
 		{{ else }} {{/* Reset count */}}
 			{{ sendMessage nil (cembed "description" (print .User.Mention " sent an incorrect number of " $Number "\nCorrect number was " $db.Next "\nStart over at 1 🙃") "color" 16711680) }}
-			{{ if $StatsCC }}{{ $UStats.Set "Total" (add $UStats.Total 1) }}{{ dbSet .User.ID "Counting" $UStats }}{{ end }}{{ dbDel 0 "Counting" }}
-			{{ if $Reactions }}{{ addReactions $IncorrectEmoji }}{{ end }}
+			{{ $db.Set "Last" (sdict "User" 204255221017214977 "Msg" 0) }}{{ $db.Set "Next" 1 }}{{ $db.Set "SecondChance" 2 }}
+			{{ dbSet 0 "Counting" $db }}
+			{{ if $StatsCC }}{{ $Silence := dbIncr .User.ID "CCount" 1 }}{{ end }}
+			{{ try }}{{ if $Reactions }}{{ addReactions $IncorrectEmoji }}{{ end }}
+			{{ catch }}{{ sendMessage $ErrorCID (print "Counting: `" .Error "`") }}{{ end }}
 		{{ end }}
 	{{ end }}
 
